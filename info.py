958ca9f77351682b86ee070e2d35235151e494b9
umangmishra6878-lang <umangmishra6878@gmail.com>
2026-01-03 17:02:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -56,12 +56,12 @@ CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Cinema_Um')
 OWNER_LNK = environ.get('OWNER_LNK', 't.me/Cinema_Hub_owner')
 UPDATE_CHANNEL_LNK = environ.get('UPDATE_CHANNEL_LNK', 'https://t.me/Cinema_Um')
 
-AUTH_CHANNEL = environ.get("AUTH_CHANNEL", "-100") # add multiple channels here, separated by single space
+AUTH_CHANNEL = environ.get("AUTH_CHANNEL", "-1002630290227") # add multiple channels here, separated by single space
 AUTH_REQ_CHANNEL = environ.get('AUTH_REQ_CHANNEL', '-1002630290227')
 
 IS_VERIFY = is_enabled('IS_VERIFY', True)
-LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-100'))
-LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-100'))
+LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-1002727865001'))
+LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1002727865001'))
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://telegra.ph/file/9ecc5d6e4df5b83424896.jpg")
 
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/SilentXBotz")
